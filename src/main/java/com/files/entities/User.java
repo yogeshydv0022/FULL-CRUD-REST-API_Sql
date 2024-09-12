f367c3b02212e712e3c7f58609b15f610e723ec7
YogeshKumar <157680046+yogeshydv0022@users.noreply.github.com>
2024-09-12 10:27:54 +0530

    Update User.java

--- src/main/java/com/files/entities/User.java
+++ src/main/java/com/files/entities/User.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,8 +13,6 @@ import lombok.Setter;
 @Entity
 @Table(name="User_REST_API")
 
-
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
